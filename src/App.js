b14daf239fc217467554ca77853a16ed1820381c
Andrew Flanery <andrew@andrewflanery.com>
2022-11-01 14:52:20 -0400

    added auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+/* src/App.js */
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
+import { Amplify, API, graphqlOperation } from 'aws-amplify'
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+const App = ({ signOut, user }) => {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div style={styles.container}>
+      <Heading level={1}>Logged in as {user.username}</Heading>
+      <Button onClick={signOut}>Sign out</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,9 +27,21 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
 
-export default App;
+const styles = {
+  container: { width: 400, margin: '0 auto', display: 'flex',
+flexDirection: 'column', justifyContent: 'center', padding: 20 },
+  todo: {  marginBottom: 15 },
+  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10,
+padding: 8, fontSize: 18 },
+  todoName: { fontSize: 20, fontWeight: 'bold' },
+  todoDescription: { marginBottom: 0 },
+  button: { backgroundColor: 'black', color: 'white', outline: 'none',
+fontSize: 18, padding: '12px 0px' }
+}
+
+
+export default withAuthenticator(App);
