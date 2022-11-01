4a679ed67ffafbfd229c3efe406442eae5388c44
Andrew Flanery <andrew@andrewflanery.com>
2022-11-01 13:47:33 -0400

    added graphql api

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "bowmaster": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
