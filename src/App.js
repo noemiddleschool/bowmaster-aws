36ce3aedf4ca99bda87e36c7b450aba0f15823f8
Andrew Flanery <andrew@andrewflanery.com>
2022-11-01 15:10:07 -0400

    updated header and other visual adjustments

--- src/App.js
+++ src/App.js
@@ -12,34 +12,17 @@ Amplify.configure(awsExports);
 const App = ({ signOut, user }) => {
   return (
     <div style={styles.container}>
-      <Heading level={1}>Logged in as {user.username}</Heading>
+      <Heading level={4}>Welcome, {user.username}</Heading>
       <Button onClick={signOut}>Sign out</Button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-	  Test changes for cont deploy.. attempt 2.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Heading level={1}>Welcome to BowMaster v1</Heading>
     </div>
   );
 }
 
 const styles = {
-  container: { width: 400, margin: '0 auto', display: 'flex',
+  container: { width: 800, margin: '0 auto', display: 'flex',
 flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  todo: {  marginBottom: 15 },
-  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10,
-padding: 8, fontSize: 18 },
-  todoName: { fontSize: 20, fontWeight: 'bold' },
-  todoDescription: { marginBottom: 0 },
-  button: { backgroundColor: 'black', color: 'white', outline: 'none',
+  button: { backgroundColor: 'blue', color: 'white', outline: 'none',
 fontSize: 18, padding: '12px 0px' }
 }
 
