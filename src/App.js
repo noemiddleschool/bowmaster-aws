9e15061a30a7bce7026a5049d00b3cd3c04bf3f2
Andrew Flanery <andrew@andrewflanery.com>
2022-11-13 15:37:26 -0500

    added user profile form to interface with dynamodb backend

--- src/App.js
+++ src/App.js
@@ -3,19 +3,76 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react'
 import { Amplify, API, graphqlOperation } from 'aws-amplify'
+import { createUser } from './graphql/mutations';
 import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
+const initialUserForm = { firstname: '', lastname: '', email: '', draw: '', handedness: '' }
+
 const App = ({ signOut, user }) => {
+  const [formState, setFormState] = useState(initialUserForm)
+  const [users, setUsers] = useState([])
+
+  function setInput(key, value) { 
+    setFormState({...formState, [key]: value})
+  }
+
+  async function addUser() {
+    try {
+      if (!formState.firstname || !formState.lastname || !formState.email) return
+      const user = { ...formState }
+      setUsers([...users, user])
+      setFormState(initialUserForm)
+      await API.graphql(graphqlOperation(createUser, {input: user}))
+    } catch (err) {
+      console.log('error creating user:', err)
+    }
+  }
+
+
   return (
     <div style={styles.container}>
       <Heading level={4}>Welcome, {user.username}</Heading>
       <Button onClick={signOut}>Sign out</Button>
       <Heading level={1}>Welcome to BowMaster v1</Heading>
       <br></br>
+      <Heading level={3}>User Profile Form</Heading>
+      <input
+        onChange={event => setInput('firstname', event.target.value)}
+        style={styles.input}
+        value={formState.firstname}
+        placeholder="First Name"
+      />
+      <input
+        onChange={event => setInput('lastname', event.target.value)}
+        style={styles.input}
+        value={formState.lastname}
+        placeholder="Last Name"
+      />
+      <input
+        onChange={event => setInput('email', event.target.value)}
+        style={styles.input}
+        value={formState.email}
+        placeholder="Email"
+      />
+      <input
+        onChange={event => setInput('draw', event.target.value)}
+        style={styles.input}
+        value={formState.draw}
+        placeholder="Draw"
+      />
+      <input
+        onChange={event => setInput('handedness', event.target.value)}
+        style={styles.input}
+        value={formState.handedness}
+        placeholder="Handedness"
+      />
+      <button style={styles.button} onClick={addUser}>Save Profile</button>
+ 
+    
       <Heading level={3}>Current Equipment Listing</Heading>
       <p>
        Sample data of equipment matching profile requirements
