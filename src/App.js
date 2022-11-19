4b2087607f9ebbb01931fdf437f0509d8c975df1
itonges <isaac.tonges@gmail.com>
2022-11-19 08:06:15 -0500

    Add header, change styling, reorganize page elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 /* src/App.js */
 import logo from './logo.svg';
+import menuIcon from './menu_icon.png'
 import './App.css';
 import React, { useEffect, useState } from 'react'
 import { Amplify, API, graphqlOperation } from 'aws-amplify'
@@ -16,8 +17,8 @@ const App = ({ signOut, user }) => {
   const [formState, setFormState] = useState(initialUserForm)
   const [users, setUsers] = useState([])
 
-  function setInput(key, value) { 
-    setFormState({...formState, [key]: value})
+  function setInput(key, value) {
+    setFormState({ ...formState, [key]: value })
   }
 
   async function addUser() {
@@ -26,74 +27,79 @@ const App = ({ signOut, user }) => {
       const user = { ...formState }
       setUsers([...users, user])
       setFormState(initialUserForm)
-      await API.graphql(graphqlOperation(createUser, {input: user}))
+      await API.graphql(graphqlOperation(createUser, { input: user }))
     } catch (err) {
       console.log('error creating user:', err)
     }
   }
 
+  const [navbarOpen, setNavbarOpen] = useState(false)
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen)
+  }
+
 
   return (
-    <div style={styles.container}>
-      <Heading level={4}>Welcome, {user.username}</Heading>
-      <Button onClick={signOut}>Sign out</Button>
-      <Heading level={1}>BowMaster</Heading>
-      <Heading level={3}>Noe Middle School Archery</Heading>
-      <br></br>
-      <Heading level={3}>User Profile Form</Heading>
-      <input
-        onChange={event => setInput('firstname', event.target.value)}
-        style={styles.input}
-        value={formState.firstname}
-        placeholder="First Name (required)"
-      />
-      <input
-        onChange={event => setInput('lastname', event.target.value)}
-        style={styles.input}
-        value={formState.lastname}
-        placeholder="Last Name (required)"
-      />
-      <input
-        onChange={event => setInput('email', event.target.value)}
-        style={styles.input}
-        value={formState.email}
-        placeholder="Email (required)"
-      />
-      <input
-        onChange={event => setInput('draw', event.target.value)}
-        style={styles.input}
-        value={formState.draw}
-        placeholder="Draw (if known)"
-      />
-      <input
-        onChange={event => setInput('handedness', event.target.value)}
-        style={styles.input}
-        value={formState.handedness}
-        placeholder="Handedness (if known)"
-      />
-      <button style={styles.button} onClick={addUser}>Save Profile</button>
-      <br></br>
-    
-      <Heading level={3}>Current Equipment Listing</Heading>
-      <p>
-       Sample data of equipment matching profile requirements
-      </p>
-      <Heading level={3}>Current Active Sessions</Heading>
-      <p>
-        Sample data from active session
-      </p>
-      <br></br>
-      <p style={styles.footer}>Bowmaster v1</p>
+
+    <div className='page'>
+      <div className="header row">
+        <p className='titleHeader'>BowMaster</p>
+        <nav className="navBar">
+          <button onClick={handleToggle}><img alt="menu icon" src={menuIcon} className="navIcon"></img></button>
+          <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
+            <li><button onClick={App}>Home</button></li>
+            <li><button onClick={signOut}>Sign out</button></li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className='content'>
+        <Heading level={3}>Noe Middle School Archery</Heading>
+        <Heading level={4}>Welcome, {user.username}</Heading>
+
+        <form>
+          <Heading level={3}>User Profile Form</Heading>
+          <input
+            onChange={event => setInput('firstname', event.target.value)}
+            value={formState.firstname}
+            placeholder="First Name (required)"
+          />
+          <input
+            onChange={event => setInput('lastname', event.target.value)}
+            value={formState.lastname}
+            placeholder="Last Name (required)"
+          />
+          <input
+            onChange={event => setInput('email', event.target.value)}
+            value={formState.email}
+            placeholder="Email (required)"
+          />
+          <input
+            onChange={event => setInput('draw', event.target.value)}
+            value={formState.draw}
+            placeholder="Draw (if known)"
+          />
+          <input
+            onChange={event => setInput('handedness', event.target.value)}
+            value={formState.handedness}
+            placeholder="Handedness (if known)"
+          />
+          <button onClick={addUser}>Save Profile</button>
+        </form>
+
+        <Heading level={3}>Current Equipment Listing</Heading>
+        <p>
+          Sample data of equipment matching profile requirements
+        </p>
+        <Heading level={3}>Current Active Sessions</Heading>
+        <p>
+          Sample data from active session
+        </p>
+        <br></br>
+        <p>Bowmaster v1</p>
+      </div>
     </div>
   );
-}
-
-const styles = {
-  container: { width: 800, margin: '0 auto', display: 'flex',
-flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  button: { backgroundColor: 'red', color: 'white', outline: 'none',
-fontSize: 18, padding: '12px 0px' },
-  footer: { justifyContent: 'center', color: 'grey', fontStyle: 'italic' }
 }
 
 
